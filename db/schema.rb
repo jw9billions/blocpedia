98f74338fcde1a3e6d5f0e6d48b511e9fb414480
Jane Wang <jw9billions@gmail.com>
2017-08-09 00:15:40 -0700

    cp3

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20170808032637) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",
+    t.datetime "updated_at",                          
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
