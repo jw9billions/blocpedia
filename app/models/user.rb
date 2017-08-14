f2e767d153781fc01cb94b51c92bd9cb278701f6
Jane Wang <jw9billions@gmail.com>
2017-08-14 14:27:36 -0700

    cp6update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          #, :confirmable
   has_many :wikis, dependent: :destroy
-
+  
+=begin
+  after_initialize do |user|
+    user.role = 'standard'
+  end
+=begin
 
 end
