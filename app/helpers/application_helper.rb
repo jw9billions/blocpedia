8b39b1edd576f1cee172ed911f5ff54bbd45447d
Jane Wang <jw9billions@gmail.com>
2017-09-02 01:41:28 -0700

    markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # https://www.youtube.com/watch?v=vPW6t-r0xzM
-  # https://github.com/tadeosss/blocipedia/commit/511f6357cd7a10eae8a2de211bcdf0e010a5990a
+
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :fenced_code_blocks, :highlight]
     Redcarpet.new(text, *options).to_html.html.safe
